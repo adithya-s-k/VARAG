130ddd3af831ebc9a9a04f74663432665bdc9be1
adithya-s-k <adithyaskolavi@gmail.com>
2024-09-27 13:10:58 +0000

    modified chunking code - Adithya S K

--- varag/chunking/__init__.py
+++ varag/chunking/__init__.py
@@ -30,26 +30,45 @@ class FixedTokenChunker(BaseChunker):
         total = 0
         for d in splits:
             _len = self._length_function(d)
-            if total + _len > self._chunk_size:
-                if total > self._chunk_size:
-                    print(
-                        f"Created a chunk of size {total}, which is longer than the specified {self._chunk_size}"
-                    )
+            if _len > self._chunk_size:
+                # If a single split is longer than chunk_size, we need to handle it separately
                 if current_doc:
-                    doc = self._join_docs(current_doc, separator)
-                    if doc is not None:
-                        docs.append(doc)
-                    while total > self._chunk_overlap or (
-                        total + _len > self._chunk_size and total > 0
-                    ):
-                        total -= self._length_function(current_doc[0])
-                        current_doc = current_doc[1:]
-            current_doc.append(d)
-            total += _len
-        doc = self._join_docs(current_doc, separator)
-        if doc is not None:
-            docs.append(doc)
-        return docs
+                    docs.append(self._join_docs(current_doc, separator))
+                    current_doc = []
+                    total = 0
+                # Split the long sentence into smaller parts
+                sub_splits = self._split_long_sentence(d, separator)
+                docs.extend(sub_splits)
+            elif total + _len > self._chunk_size:
+                if current_doc:
+                    docs.append(self._join_docs(current_doc, separator))
+                current_doc = [d]
+                total = _len
+            else:
+                current_doc.append(d)
+                total += _len
+        if current_doc:
+            docs.append(self._join_docs(current_doc, separator))
+        return [doc for doc in docs if doc is not None]
+
+    def _split_long_sentence(self, sentence: str, separator: str) -> List[str]:
+        words = sentence.split()
+        chunks = []
+        current_chunk = []
+        current_length = 0
+        for word in words:
+            word_len = self._length_function(word)
+            if current_length + word_len > self._chunk_size:
+                if current_chunk:
+                    chunks.append(" ".join(current_chunk))
+                current_chunk = [word]
+                current_length = word_len
+            else:
+                current_chunk.append(word)
+                current_length += word_len
+        if current_chunk:
+            chunks.append(" ".join(current_chunk))
+        return chunks
 
     def _join_docs(self, docs: List[str], separator: str) -> Optional[str]:
         text = separator.join(docs)
