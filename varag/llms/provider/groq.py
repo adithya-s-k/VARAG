73e7b59d306e2f4c4095bbdc4942439957419a3d
adithya-s-k <adithyaskolavi@gmail.com>
2024-09-27 18:36:54 +0000

    base llm added - Adithya S K

--- varag/llms/provider/groq.py
+++ varag/llms/provider/groq.py
@@ -1,6 +1,5 @@
 import os
-from typing import Optional, List, Union, Dict
-import warnings
+from typing import Optional, List, Union
 from PIL import Image
 from openai import OpenAI
 from varag.llms import BaseLLM
@@ -8,40 +7,58 @@ from varag.llms import BaseLLM
 
 class GroqLLM(BaseLLM):
     DEFAULT_BASE_URL = "https://api.groq.com/openai/v1"
-    DEFAULT_MODEL = "mixtral-8x7b-32768"
-
-    MODELS = [
-        "gemma2-9b-it",
-        "gemma-7b-it",
-        "llama3-groq-70b-8192-tool-use-preview",
-        "llama3-groq-8b-8192-tool-use-preview",
-        "llama-3.1-70b-versatile",
-        "llama-3.1-8b-instant",
-        "llama-3.2-1b-preview",
-        "llama-3.2-3b-preview",
-        "llama-3.2-11b-text-preview",
-        "llama-3.2-90b-text-preview",
-        "llama-guard-3-8b",
-        "llava-v1.5-7b-4096-preview",
-        "llama3-70b-8192",
-        "llama3-8b-8192",
-        "mixtral-8x7b-32768",
-    ]
+    DEFAULT_MODEL = "llava-v1.5-7b-4096-preview"
+    DEFAULT_MAX_IMAGES = 5
 
     def __init__(
         self,
         api_key: Optional[str] = None,
         base_url: Optional[str] = None,
         model: str = DEFAULT_MODEL,
+        max_images: int = DEFAULT_MAX_IMAGES,
     ):
-        api_key = api_key or os.getenv("GROQ_API_KEY")
-        super().__init__(api_key, base_url or self.DEFAULT_BASE_URL, model)
+        self.api_key = api_key or os.getenv("GROQ_API_KEY")
+        self.base_url = base_url or self.DEFAULT_BASE_URL
+        self.model = model
+        self.max_images = max_images
+        self.client = self._initialize_client()
 
-    def list_models(self) -> List[Dict]:
-        url = f"{self.base_url}/models"
-        headers = {
-            "Authorization": f"Bearer {self.api_key}",
-            "Content-Type": "application/json",
+    def _initialize_client(self) -> OpenAI:
+        return OpenAI(api_key=self.api_key, base_url=self.base_url)
+
+    def query(
+        self,
+        query: str,
+        context: str,
+        system_prompt: str,
+        max_tokens: Optional[int] = None,
+    ) -> str:
+        context = f"""
+        
+        Given the following Context 
+        
+        Context
+        =============================
+         
+        {context}
+        
+        =============================
+        
+        
+        """
+
+        completion_params = {
+            "model": self.model,
+            "messages": [
+                {"role": "system", "content": system_prompt},
+                {"role": "user", "content": str(context + query)},
+            ],
         }
-        response = self.client.get(url, headers=headers)
-        return response.json()
+        if max_tokens is not None:
+            completion_params["max_tokens"] = max_tokens
+
+        response = self.client.chat.completions.create(**completion_params)
+        return response.choices[0].message.content
+
+    def __call__(self, image: Union[str, Image.Image], query: str) -> str:
+        return self.response(query, image)
