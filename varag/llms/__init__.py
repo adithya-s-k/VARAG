0172c86025c0cf0668ab730849cfe5e84eb396c9
adithya-s-k <adithyaskolavi@gmail.com>
2024-09-27 22:02:23 +0000

    removed groq vlm as it supports only 1 image input - Adithya S K

--- varag/llms/__init__.py
+++ varag/llms/__init__.py
@@ -1,5 +1,8 @@
 from .base import BaseLLM
 from .provider.openai import OpenAILLM as OpenAI
-from .provider.groq import GroqLLM as Groq
 
-__all__ = ["BaseLLM", "OpenAI", "Mistral", "Groq"]
+
+__all__ = [
+    "BaseLLM",
+    "OpenAI",
+]
--- varag/vlms/__init__.py
+++ varag/vlms/__init__.py
@@ -1,5 +1,4 @@
 from .base import BaseVLM
 from .provider.openai import OpenAIVLM as OpenAI
-from .provider.groq import GroqVLM as Groq
 
-__all__ = ["BaseVLM", "OpenAI", "Mistral", "Groq"]
+__all__ = ["BaseVLM", "OpenAI"]
